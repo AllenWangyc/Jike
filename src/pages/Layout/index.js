1a3fb815111f2871f0b9682b6b644eaa68976944
Allen Wang <a.wangyc@gmail.com>
2023-12-19 20:01:29 +0800

    fetch user info

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -6,33 +6,55 @@ import {
   LogoutOutlined
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchUserInfo } from '@/store/modules/user'
 
 const { Header, Sider } = Layout
 const items = [
   {
     label: 'Home',
-    key: '1',
+    key: '/',
     icon: <HomeOutlined />
   },
   {
     label: 'Article',
-    key: '2',
+    key: '/article',
     icon: <DiffOutlined />
   },
   {
     label: 'Publish',
-    key: '3',
+    key: '/publish',
     icon: <EditOutlined />
   },
 ]
 const GeekLayout = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const selectedKey = location.pathname
+
+  // trigger action of userInfo
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchUserInfo())
+  }, [dispatch])
+  const name = useSelector(state => state.user.userInfo.name)
+
+  /**
+   * 
+   * @param {object} route an object that contain all information of the clicked item
+   */
+  const onMenuClick = (route) => {
+    navigate(route.key)
+  }
+
   return (
     <Layout>
       <Header className='header'>
         <div className='logo' />
         <div className='user-info'>
-          <span className='user-name'>Name-Test</span>
+          <span className='user-name'>{name}</span>
           <span className='user-logout'>
             <Popconfirm title="Do you confirm to exit?" okText='Yes' cancelText='No'>
               <LogoutOutlined /> Exit
@@ -45,9 +67,10 @@ const GeekLayout = () => {
           <Menu
             mode='inline'
             theme='dark'
-            defaultSelectedKeys={['1']}
+            selectedKeys={selectedKey}
             items={items}
             style={{ height: '100%', borderRight: 0 }}
+            onClick={onMenuClick}
           ></Menu>
         </Sider>
         <Layout className='layout-content' style={{ padding: 20 }}>
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,18 +5,24 @@ import { setToken as _setToken, getToken } from '@/utils'
 const userStore = createSlice({
   name: 'user',
   initialState: {
-    token: getToken() || ''
+    token: getToken() || '',
+    userInfo: {}
   },
   reducers: {
     setToken(state, action) {
       state.token = action.payload
       state.token = _setToken(action.payload)
+    },
+    setUserInfo(state, action) {
+      state.userInfo = action.payload
     }
   }
 })
 
-const { setToken } = userStore.actions
+const { setToken, setUserInfo } = userStore.actions
 
+
+// async methods
 /**
  * 
  * @param {object} loginForm 
@@ -31,8 +37,17 @@ const fetchLogin = (loginForm) => {
   }
 }
 
+const fetchUserInfo = () => {
+  return async (dispatch) => {
+    // 1. send request 
+    const res = await request.get('/user/profile')
+    // 2. invoke sync method to set userInfo
+    dispatch(setUserInfo(res.data))
+  }
+}
+
 const userReducer = userStore.reducer
 
-export { setToken, fetchLogin }
+export { setToken, fetchLogin, fetchUserInfo }
 
 export default userReducer
