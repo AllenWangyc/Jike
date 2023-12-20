cfb09519bfd6b282077db5287c8b84aa845d61c0
Allen Wang <a.wangyc@gmail.com>
2023-12-20 11:05:29 +0800

    publish instructure

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -1,6 +1,77 @@
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  Input,
+  Upload,
+  Space,
+  Select
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const { Option } = Select
+
 const Publish = () => {
   return (
-    <div>Publish Page</div>
+    <div className='publish'>
+      <Card
+        title={
+          <Breadcrumb items={[
+            { title: <Link to={'/'}>Home</Link> },
+            { title: 'Publish' }
+          ]}
+          />
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+        >
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[{
+              required: true,
+              message: 'Please enter the title'
+            }]}
+          >
+            <Input placeholder='Enter the article title' style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="Channel"
+            name="channel_id"
+            rules={[{
+              required: true,
+              message: 'Please select a channel'
+            }]}
+          >
+            <Select placeholder='Select a channel' style={{ width: 400 }}>
+              <Option value={0}>Recommendation</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Content"
+            name="content"
+            rules={[{
+              required: true,
+              message: 'Please enter the content'
+            }]}
+          ></Form.Item>
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size='large' type='primary' htmlType='submit'>
+                Publish
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   )
 }
 
